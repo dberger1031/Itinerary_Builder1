bbb461a7ee77689923c7cd7d4d42b5459250f0bb
dberger1031 <dberger1031@gmail.com>
2026-05-22 14:20:14 -0400

    Change base path in Vite config

--- chronos-itinerary-planner/vite.config.ts
+++ chronos-itinerary-planner/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Itinerary_Builder/',
+  base: '/Itinerary_Builder1/',
 })
